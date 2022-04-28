2126046edf93d924b36c68424a80e558631ecd29
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2022-04-28 10:56:31 +0100

    cambios

---
+++ Assets/Textures/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 Offset;
+
+    public float Smoothing = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(Target.position + Offset, transform.position, Smoothing * Time.deltaTime); 
+    }
+}
