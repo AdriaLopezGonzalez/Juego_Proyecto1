698a3fcf26280ccff447ae6f6ea8e546304b0494
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2022-04-29 11:08:19 +0100

    cambios

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 3f;
+
+    private Vector2 moveInput;
+    private Rigidbody2D playerRigidBody;
+
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        playerRigidBody.MovePosition(playerRigidBody.position + moveInput * speed * Time.fixedDeltaTime);
+    }
+}
