c9e40468362d377a6f295a5db3e1428d393f71b4
Adri-Romero <aromerod@edu.tecnocampus.cat>
2022-05-18 16:13:57 +0200

    Boomerang

---
+++ Assets/scripts/attack scripts/BoomerangGun.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomerangGun : weapon
+{
+    public boomerang boomerangPrefab;
+    public Transform FirePoint;
+    public float speed = 5;
+
+    private float cooldownShoot = 0f;
+    private float lastShoot;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            TryFire();
+        }
+    }
+
+    void Fire()
+    {
+        var stinger = Instantiate(boomerangPrefab, FirePoint.position, FirePoint.rotation);
+
+        stinger.Init(speed);
+    }
+
+    public override void TryFire()
+    {
+        if (CanShoot())
+        {
+            Fire();
+            cooldownShoot = 0.5f;
+            lastShoot = Time.time;
+        }
+    }
+
+    private bool CanShoot()
+    {
+        return (lastShoot + cooldownShoot) < Time.time;
+    }
+}
---
+++ Assets/scripts/attack scripts/boomerang.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boomerang : MonoBehaviour
+{
+    Rigidbody2D _rigidbody;
+    private bool returning = false;
+    public float timer;
+    private float velocity;
+
+    private void Update()
+    {
+        timer += 1.0f + Time.deltaTime;
+        if (timer >= 1000f)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+    public void Init(float speed)
+    {
+        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.velocity = transform.right * speed;
+        
+        velocity = speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.collider.CompareTag("player") && !collision.collider.CompareTag("key") && !collision.collider.CompareTag("boomerang"))
+        {
+            _rigidbody.velocity = transform.right * -velocity;
+            returning = true;
+
+        }
+        if (collision.collider.CompareTag("player") && returning==true)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
