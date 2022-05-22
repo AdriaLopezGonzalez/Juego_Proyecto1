4796ba36bfb9125f26a375bc40c188e52eb63969
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2022-05-22 18:05:15 +0100

    enemigo a distancia

---
+++ Assets/scripts/enemy scripts/LookAtPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    public GameObject player;
+    void Update()
+    {
+        var direction = (player.transform.position) - transform.position;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
